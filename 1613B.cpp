f5d3c910b909323ecdb0b7b677afdf277c7d3af9
Asish Kumar <officialasishkumar@gmail.com>
2022-02-01 13:20:26 +0530

    Added sol 1613/B

---
+++ 1613B.cpp
@@ -0,0 +1,43 @@
+/*
+Author : Asish Kumar
+*/
+// Problem link : https://codeforces.com/contest/1613/problem/B
+
+//   Educational Codeforces Round 118 (Rated for Div. 2)
+//                     B. Absent Remainder
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve() {
+	ll h1, d1, h2, d2, k, w, a;
+	cin >> h1 >> d1 >> h2 >> d2 >> k >> w >> a;
+	if(ceil(((double)h1+k*a)/d2) >= ceil((double)h2/d1)){
+		cout << "YES" << endl;
+		return;
+	} 
+	for(ll i = 1; i <= k; i++){
+		ll A = ceil(((double)h1+(k-i)*a)/d2);
+		ll B = ceil((double)h2/(d1 + i*w));
+		if(A >= B){
+			cout << "YES" << endl;
+			return; 
+		}
+	}
+	cout << "NO" << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t; cin >> t;
+	while (t--) {
+		solve();
+	}
+	// solve();
+}
